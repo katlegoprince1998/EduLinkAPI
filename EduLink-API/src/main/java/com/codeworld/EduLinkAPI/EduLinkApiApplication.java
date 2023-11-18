73a92d62a90b8e3099210e00297915c27811b317
katlegoprince1998 <princekatlego357@gmail.com>
2023-11-18 03:55:01 +0200

    Spring boot files

---
+++ EduLink-API/src/main/java/com/codeworld/EduLinkAPI/EduLinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.codeworld.EduLinkAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduLinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduLinkApiApplication.class, args);
+	}
+
+}
---
+++ EduLink-API/src/test/java/com/codeworld/EduLinkAPI/EduLinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeworld.EduLinkAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduLinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
